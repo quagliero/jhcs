0ddf95fd6200fbc2923c4266521bb111472aa50b
thd <tharisondenby@godaddy.com>
2020-05-17 22:19:53 +0100

    feat: webp fallback

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import lewisChessWebp from './lewis-chess.webp';
+import lewisChessJpg from './lewis-chess.jpg';
 import './Header.css';
 
 const Header = ({
@@ -8,6 +10,11 @@ const Header = ({
 
   return (
     <header className="header">
+      <picture className="headerBg">
+        <source srcset={lewisChessWebp} type="image/webp"/>
+        <source srcset={lewisChessJpg} type="image/jpeg"/>
+        <img src={lewisChessJpg} alt="Lewis Chessmen"/>
+      </picture>
       <h1>{title || "♟️ John Harrison Championship Series ♟️"}</h1>
       {<h2>{subtitle || "Harison-Denby vs. Harrison, Correspondence 2020."}</h2>}
     </header>
